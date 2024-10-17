8cd6297dc615e9369ca45fa9d2a83d9d98e1c896
Marcos Paulo Evers <marcospauloeversc@gmail.com>
2024-10-17 19:44:56 -0300

    Update run_experiment.c

--- user/run_experiment.c
+++ user/run_experiment.c
@@ -2,13 +2,55 @@
 #include <kernel/metrics.h>
 #include <user/user.h>
 
-int main(int argc, char** argv) {
-  if (fork() == 0) {
-    char* argv[] = {"io_bound", "tmp", 0};
-    exec("io_bound", argv);
-  } else {
-    wait(0);
-    printf("%ld\n", mtime(TIMEIO));
+#define NROUNDS 30
+#define NPROC 20
+
+void
+spawn_cpubound(uint ncpu)
+{
+  for (int i = 0; i < ncpu; i++) {
+    if (fork() != 0) continue;
+
+    char* argv[] = {"cpu_bound", 0};
+    exec(argv[0], argv);
+  }
+}
+
+void
+spawn_iobound(uint nio)
+{
+  for (int i = 0; i < nio; i++) {
+    if (fork() != 0) continue;
+
+    char name[] = "tmp_##";
+    name[4] = i/10 + '0';
+    name[5] = i%10 + '0';
+
+    char* argv[] = {"io_bound", name, 0};
+    exec(argv[0], argv);
+  }
+}
+
+int
+main(int argc, char** argv)
+{
+  for (int i = 1; i <= NROUNDS; i++) {
+    uint ncpu = rng_range(6, 14);
+    uint nio = 20 - ncpu;
+
+    printf("[ROUND #%d] ", i);
+    printf("%d cpu bound process; ", ncpu);
+    printf("%d io bound process\n", nio);
+
+    // TODO: reset metrics
+
+    spawn_cpubound(ncpu);
+    spawn_iobound(nio);
+
+    for (uint num = 0; num < NPROC; num++)
+      wait(0);
+
+    // TODO: calc and show system metrics
   }
   exit(0);
 }
