514063d5c38f820580273dfab9007cb9529e1fa2
Marcos Paulo Evers <marcospauloeversc@gmail.com>
2024-10-23 17:27:09 -0300

    Change bcache algorithm to the clock

--- kernel/bio.c
+++ kernel/bio.c
@@ -27,10 +27,8 @@ struct {
   struct spinlock lock;
   struct buf buf[NBUF];
 
-  // Linked list of all buffers, through prev/next.
-  // Sorted by how recently the buffer was used.
-  // head.next is most recent, head.prev is least.
-  struct buf head;
+  // clock head
+  uint head;
 } bcache;
 
 void
@@ -40,16 +38,19 @@ binit(void)
 
   initlock(&bcache.lock, "bcache");
 
-  // Create linked list of buffers
-  bcache.head.prev = &bcache.head;
-  bcache.head.next = &bcache.head;
+  // Initialize Clock Pointer
+  bcache.head = 0;
   for(b = bcache.buf; b < bcache.buf+NBUF; b++){
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
     initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
+    b->flag = 1;
   }
+}
+
+static void
+incmod(uint* x, uint mod)
+{
+  (*x)++;
+  if (*x == mod) *x = 0;
 }
 
 // Look through buffer cache for block on device dev.
@@ -58,12 +59,13 @@ binit(void)
 static struct buf*
 bget(uint dev, uint blockno)
 {
+  uint nhead, used;
   struct buf *b;
 
   acquire(&bcache.lock);
 
   // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
+  for(b = bcache.buf; b < bcache.buf+NBUF; b++){
     if(b->dev == dev && b->blockno == blockno){
       b->refcnt++;
       release(&bcache.lock);
@@ -73,13 +75,20 @@ bget(uint dev, uint blockno)
   }
 
   // Not cached.
-  // Recycle the least recently used (LRU) unused buffer.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
-    if(b->refcnt == 0) {
+  for(nhead = bcache.head, used = 0; used < NBUF; incmod(&nhead, NBUF)){
+    b = &bcache.buf[nhead];
+
+    if (b->refcnt > 0) {
+      used++;
+    } else if (b->flag == 1) {
+      b->flag = 0;
+    } else {
+      bcache.head = nhead;
       b->dev = dev;
       b->blockno = blockno;
       b->valid = 0;
       b->refcnt = 1;
+      b->flag = 1;
       release(&bcache.lock);
       acquiresleep(&b->lock);
       return b;
@@ -123,15 +132,15 @@ brelse(struct buf *b)
 
   acquire(&bcache.lock);
   b->refcnt--;
-  if (b->refcnt == 0) {
-    // no one is waiting for it.
-    b->next->prev = b->prev;
-    b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
-  }
+  // if (b->refcnt == 0) {
+  //   // no one is waiting for it.
+  //   b->next->prev = b->prev;
+  //   b->prev->next = b->next;
+  //   b->next = bcache.head.next;
+  //   b->prev = &bcache.head;
+  //   bcache.head.next->prev = b;
+  //   bcache.head.next = b;
+  // }
   
   release(&bcache.lock);
 }
